2e442524d43f253621dd4fe39ffc0ba29e4a8d3e
PlusNguyn <tranthainguyenpntlop12c8@gmail.com>
2025-03-03 17:02:08 +0700

    update

--- language_assistant/homepage/urls.py
+++ language_assistant/homepage/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import home, login
+from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('login/', login, name='login')
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('courses/', courses, name='courses'),
+    path('team/', team, name='team'),
+    path('testimonial/', testimonial, name='testimonial'),
+    path('instructor/', instructor, name='instructor'),
+    path('single/', single, name='single'),
 ]
--- language_assistant/homepage/views.py
+++ language_assistant/homepage/views.py
@@ -6,3 +6,27 @@ def home(request):
 
 def login(request):
     return render(request, 'homepage/login.html')
+
+def signup(request):
+    return render(request, 'homepage/signup.html')
+
+def about(request):
+    return render(request, 'homepage/about.html')
+
+def contact(request):
+    return render(request, 'homepage/contact.html')
+
+def courses(request):
+    return render(request, 'homepage/courses.html')
+
+def team(request):
+    return render(request, 'homepage/team.html')
+
+def testimonial(request):
+    return render(request, 'homepage/testimonial.html')
+
+def instructor(request):
+    return render(request, 'homepage/instructor.html')
+
+def single(request):
+    return render(request, 'homepage/single.html')
